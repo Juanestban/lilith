69d31d1c4c9a0ae001fe6bb5e4fe819193f05d5c
Juanestban <juanestban000@gmail.com>
2022-06-25 10:46:24 -0500

    chore: settings the tests to notipad

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ import { getJestProjects } from '@nrwl/jest';
 
 // Sync object
 /** @type {import('@jest/types').Config.InitialOptions} */
-export default {
-  projects: getJestProjects(),
-  verbose: true,
-};
+module.exports = { projects: [...getJestProjects()] };
