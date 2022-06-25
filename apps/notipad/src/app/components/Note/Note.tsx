2c4bdc1cb4d806491afca46b0e1df58033bf5052
Juanestban <juanestban000@gmail.com>
2022-06-25 14:47:39 -0500

    refactor: to normal state

--- apps/notipad/src/app/components/Note/Note.tsx
+++ apps/notipad/src/app/components/Note/Note.tsx
@@ -2,14 +2,14 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
-import { Note } from '../../interfaces';
+import { Note as INote } from '../../interfaces';
 import { useNoteContext } from '../../contexts';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../Button';
 
 import s from './Note.module.css';
 
-interface NoteProps extends Note {
+interface NoteProps extends INote {
   // ...
 }
 
